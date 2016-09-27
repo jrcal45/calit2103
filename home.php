de39aa91be596308ec0a82a4acc18230d811c463
jrcal45 <caljohnrodri@gmail.com>
2016-09-27 14:15:21 +0800

    added button

--- home.php
+++ home.php
@@ -3,6 +3,7 @@
 <link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
 <body>
+<button class='btn btn-danger'><span class='glyphicon glyphicon-user'></span></button>
 </body>
 </html>
 <script src="js/jquery.min.js"></script>
